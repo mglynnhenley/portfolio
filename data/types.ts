80064409644636a800406a5ffaf2b19f2d1a8340
mglynnhenley <matildaglynnhenley@outlook.com>
2025-11-01 22:36:19 +0000

    feat: define data types for content

---
+++ data/types.ts
@@ -0,0 +1,25 @@
+export interface Project {
+  title: string;
+  description: string;
+  link?: string;
+  tags?: string[];
+}
+
+export interface Experience {
+  title: string;
+  company: string;
+  period: string;
+  description: string;
+}
+
+export interface Event {
+  title: string;
+  link?: string;
+}
+
+export interface PersonalInfo {
+  name: string;
+  title: string;
+  email: string;
+  bio: string[];
+}
